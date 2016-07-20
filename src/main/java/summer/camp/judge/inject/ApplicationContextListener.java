3cba346d1d8391b5d59e92947d9dbcb42144ce4a
hristoara <h.arabadjiev96@gmail.com>
2016-07-20 16:57:56 +0300

    added resources and validators

--- src/main/java/summer/camp/judge/inject/ApplicationContextListener.java
+++ src/main/java/summer/camp/judge/inject/ApplicationContextListener.java
@@ -9,15 +9,18 @@ import javax.servlet.ServletContextEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import summer.camp.judge.resources.GeneralExceptionHandler;
-import summer.camp.judge.resources.GsonMessageBodyHandler;
-import summer.camp.judge.resources.RolesPublicService;
-import summer.camp.judge.resources.TaskResource;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.persist.PersistService;
 import com.google.inject.servlet.GuiceServletContextListener;
+
+import summer.camp.judge.resources.GeneralExceptionHandler;
+import summer.camp.judge.resources.GsonMessageBodyHandler;
+import summer.camp.judge.resources.RolesPublicService;
+import summer.camp.judge.resources.SolutionResource;
+import summer.camp.judge.resources.TaskResource;
+import summer.camp.judge.resources.TestCaseResource;
+import summer.camp.judge.resources.UserResource;
 
 /**
  * This class handles the initialization of all Guice modules and all REST API
@@ -70,6 +73,9 @@ public class ApplicationContextListener extends GuiceServletContextListener {
 
 		// Services protected with the Admin role
 		getSingletons().add(injector.getInstance(TaskResource.class));
+		getSingletons().add(injector.getInstance(UserResource.class));
+		getSingletons().add(injector.getInstance(TestCaseResource.class));
+		getSingletons().add(injector.getInstance(SolutionResource.class));
 
 		// Public services
 		getSingletons().add(injector.getInstance(RolesPublicService.class));
---
+++ src/main/java/summer/camp/judge/resources/TestCaseResource.java
@@ -0,0 +1,134 @@
+package summer.camp.judge.resources;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import summer.camp.judge.commons.UnitOfWorkUtils;
+import summer.camp.judge.dao.TestCaseDao;
+import summer.camp.judge.entities.TestCase;
+import summer.camp.judge.validation.TestCaseValidator;
+
+/**
+ * Service for educations
+ */
+@Singleton
+@Path("/protected/admin/testcase")
+public class TestCaseResource extends AbstractCRUDService<Long, TestCase> {
+
+	private static final String ERROR_THERE_IS_NO_TASK_WITH_TASK_ID_MESSAGE = "There is no task with [taskId={0}]";
+
+	/**
+	 * Constructor
+	 *
+	 * @param taskDao
+	 * @param taskValidator
+	 * @param unitOfWorkUtils
+	 */
+	@Inject
+	public TestCaseResource(TestCaseDao testcaseDao, TestCaseValidator testcaseValidator, UnitOfWorkUtils unitOfWorkUtils) {
+		super(testcaseDao, testcaseValidator, unitOfWorkUtils);
+	}
+
+	/**
+	 * Returns a list of all tasks
+	 *
+	 * @return a list of all tasks
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<TestCase> getTestsCase() {
+		return getAll();
+	}
+
+	/**
+	 * Returns a task specified by the id path parameter
+	 *
+	 * @param id
+	 * @return a task specified by the id path parameter
+	 */
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public TestCase getEducation(@PathParam("id") final Long id) {
+		return get(id);
+	}
+
+	/**
+	 * Adds new task
+	 *
+	 * @param task
+	 * @return HTTP 201 CREATED if the task was successfully added or
+	 *         HTTP 400 BAD REQUEST if something went wrong
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addEducation(TestCase task) {
+		return add(task);
+	}
+
+	/**
+	 * Updates existing task
+	 *
+	 * @param id
+	 * @param task
+	 * @return HTTP 204 NO CONTENT if the update was successful or
+	 *         HTTP 404 NOT FOUND if there was no such task
+	 */
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateTestCase(@PathParam("id") final Long id, TestCase testcase) {
+		return update(id, testcase);
+	}
+
+	/**
+	 * Deletes existing task
+	 *
+	 * @param id
+	 * @return HTTP 204 NO CONTENT if the deletion was successful or
+	 *         HTTP 404 NOT FOUND if there was no such education
+	 */
+	@DELETE
+	@Path("/{id}")
+	public Response deleteEducation(@PathParam("id") final Long id) {
+		return delete(id);
+	}
+
+	/**
+	 * Returns the count of all tasks
+	 *
+	 * @return the count of all tasks
+	 */
+	@GET
+	@Path("/count")
+	public Long count() {
+		return countAll();
+	}
+
+	@Override
+	protected String getNotFoundMessage(Long id) {
+		return MessageFormat.format(ERROR_THERE_IS_NO_TASK_WITH_TASK_ID_MESSAGE, id);
+	}
+
+	@Override
+	protected void updateEntityProperties(TestCase persistedEntity, TestCase entity) {
+		persistedEntity.setInput(entity.getInput());
+		persistedEntity.setOutput(entity.getOutput());
+
+	}
+
+}
